98f8b0632df648eb1504791fc1aacb9e06e28866
211250165 <211250165@smail.nju.edu.cn>
2023-08-24 22:12:04 +0800

    update: api for user

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello_world():
     print("Hello, World!")
     return "<p>Hello, World!</p>"
 
-@app.route("/class/<order>")
+@app.route("/classinfo/<order>")
 def get_class_order(order):
     print(order)
     return {
@@ -19,4 +19,13 @@ def get_class_order(order):
         "time": ["周一 1-2节", "周三 3-4节"],
         "rate": 4.5,
         "location": "教学楼A101",
+    }
+    
+@app.route("/userinfo/<order>")
+def get_user_order(order):
+    print(order)
+    return {
+        "id": "J221525",
+        "name": "胥昱泉",
+        "avatar": "https://files.lsmcloud.top/blog26111653219843_.jpg"
     }
